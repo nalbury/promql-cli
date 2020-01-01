3ee3c89aa0832c1c0afa7be981cddfaa357f7dd4
Nick Albury <nickalbury@gmail.com>
2020-01-01 17:04:01 -0500

    rm stdout on successful config read

--- cmd/root.go
+++ cmd/root.go
@@ -369,7 +369,7 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
 	}
 }
